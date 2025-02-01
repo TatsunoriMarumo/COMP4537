94f45b233ffe6e0336717c2084dbb175c7433cf2
TatsunoriMarumo <t.maru0910@gmail.com>
2025-02-01 13:21:10 -0800

    Create lab03 part B

---
+++ lab03_partA/app.js
@@ -0,0 +1,7 @@
+const math = require("./modules/math")
+
+const name = "Tatsunori"
+const a = 1
+const b = 2
+
+console.log(`Hello ${name}\n${a} + ${b} = ${math.add(a, b)}\n${a} - ${b} = ${math.subtract(a, b)}`)
